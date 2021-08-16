10b9af4907bf3a4ff67bf4ff92dfb4b40772301a
davidAg9 <shane_alpha9@outlook.com>
2021-08-16 07:45:45 +0000

    Initial Commit

---
+++ chatMe/chatMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatMeApp.swift
+//  chatMe
+//
+//  Created by Dave Agyakwa on 16/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct chatMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
