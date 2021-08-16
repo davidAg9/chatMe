48106e92646bde90ff848b5d06e6fcaf0471f6ff
davidAg9 <shane_alpha9@outlook.com>
2021-08-16 08:10:25 +0000

    refactored [NewConversationView] for consistency , change the landscape setting in info.plist and rebuild project

--- chatMe/views /NewConversationView.swift
+++ chatMe/views /NewConversationView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct newConversationView: View {
+struct NewConversationView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct newConversationView_Previews: PreviewProvider {
+struct NewConversationView_Previews: PreviewProvider {
     static var previews: some View {
-        newConversationView()
+        NewConversationView()
     }
 }
